cd76ccba478ac5c488a7522f4721c23e259f368b
LuigiG34 <luigigandemer6@gmail.com>
2022-07-08 11:32:54 +0200

    Routeur

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+
+require './controllers/LivreController.php';
+
+define("URL", str_replace("index.php", "", (isset($_SERVER['HTTPS']) ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']));
+$a = new LivreController();
+
+try {
+    if (!isset($_GET['page'])) {
+        require "views/index.php";
+    } else {
+        $url = explode("/", filter_var($_GET['page']), FILTER_SANITIZE_URL);
+        switch ($url[0]) {
+            case 'accueil':
+                require "views/index.php";
+                break;
+            case 'livres':
+                if(!isset($url[1])){
+                    $a->afficherLivres();
+                    $array = LivreManager::getArray();
+                    require "views/livres.php";
+                }
+                if (isset($url[1])) {
+                    switch ($url[1]) {
+                        case 'read':
+                            $book = $a->afficherLivresById(intval($url[2]));
+                            require "views/crud/read.php";
+                            break;
+                        case 'modify':
+                            $book = $a->afficherLivresById(intval($url[2]));
+                            require "views/crud/modify.php";
+                            break;
+                        case 'create':
+                            require "views/crud/create.php";
+                            break;
+                        case 'delete':
+                            $a->supprimerLivre(intval($url[2]));
+                            $manager = new LivreManager();
+                            $manager->supprimerLivreBD($url[2]);
+                            break;
+                        case 'valider':
+                            $img = $a->ajoutLivreValidation();
+                            $manager = new LivreManager();
+                            $manager->ajoutLivreBD($_POST['titre'],$_POST['pages'], $img);
+                            break;
+                        case 'modifValider':
+                            break;
+                        default:
+                            http_response_code(404);
+                            require "views/404.php";
+                            break;
+                    }
+                }
+                break;
+            default:
+                http_response_code(404);
+                require "views/404.php";
+                break;
+        }
+    }
+} catch (Exception $e) {
+    http_response_code(404);
+    require "views/404.php";
+}
